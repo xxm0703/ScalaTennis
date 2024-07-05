765ade094fd8e2af2f4c62afe03590cddca8847d
Sonya Nikolova <snikolova27@gmail.com>
2024-07-05 16:21:52 +0300

    delete shopping cart

--- src/main/scala/fmi/reservation/ShoppingCart.scala
+++
@@ -1,8 +0,0 @@
-package fmi.reservation
-
-import io.circe.Codec
-import sttp.tapir.Schema
-
-case class TennisCart(orderLines: List[OrderLine] = List.empty) derives Codec, Schema:
-  def add(orderLine: OrderLine): TennisCart =
-    TennisCart(orderLine :: orderLines)
